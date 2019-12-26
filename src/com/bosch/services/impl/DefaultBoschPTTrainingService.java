89c8d40a283aacd5e37e385c309988b912020333
MazZa <mazzabosch@gmail.com>
2019-12-26 11:55:44 +0100

    Moj prvi commit

--- src/com/bosch/services/impl/DefaultBoschPTTrainingService.java
+++ src/com/bosch/services/impl/DefaultBoschPTTrainingService.java
@@ -1,10 +1,17 @@
 package com.bosch.services.impl;
 
+import com.bosch.services.BoschPTTrainingService;
+
 public class DefaultBoschPTTrainingService implements BoschPTTrainingService {
 
     @Override
     public void practiceGit() {
         System.out.println("Git rocks");
     }
-  
+
+    @Override
+    public void djihadjihacetirinoge() {
+
+    }
+
 }
---
+++ src/com/bosch/services/mazic/Product.java
@@ -0,0 +1,9 @@
+package com.bosch.services.mazic;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private String quantity;
+
+}
