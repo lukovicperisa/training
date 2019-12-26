20da2db2a5149c43d0567a7a8e7436d42c6b279d
FIXED-TERM Rankovic Nera (RBSM/PJ-DSC3.1) <rna1bg@bosch.com>
2019-12-26 11:50:57 +0100

    TASK -4: Create new models -Car, City, Student

---
+++ src/rankovic/CarModel.java
@@ -0,0 +1,52 @@
+package rankovic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+	public class Car {
+		private String make;
+		private String modell;
+		private String fuel;
+		private int seats;
+		private double price;
+		private String color;
+		public String getMake() {
+			return make;
+		}
+		public void setMake(String make) {
+			this.make = make;
+		}
+		public String getModell() {
+			return modell;
+		}
+		public void setModell(String modell) {
+			this.modell = modell;
+		}
+		public String getFuel() {
+			return fuel;
+		}
+		public void setFuel(String fuel) {
+			this.fuel = fuel;
+		}
+		public int getSeats() {
+			return seats;
+		}
+		public void setSeats(int seats) {
+			this.seats = seats;
+		}
+		public double getPrice() {
+			return price;
+		}
+		public void setPrice(double price) {
+			this.price = price;
+		}
+		public String getColor() {
+			return color;
+		}
+		public void setColor(String color) {
+			this.color = color;
+		}
+	}
+		
+	
