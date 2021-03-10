6f5e114e0747e13db2ea40d004943ad86c14e514
Igor <igor.stojanovic.nis@outlook.com>
2021-03-10 21:36:54 +0100

    feature/STOJANOVIC_1 first commit

---
+++ src/com/bosch/services/stojanovic/DemoDrill.java
@@ -0,0 +1,11 @@
+package com.kakarot.zaBrisanje.stojanovic;
+
+public class DemoDrill {
+
+    public static void drill(){
+        while(true){
+            System.out.println("DRILL xD");
+        }
+    }
+
+}
